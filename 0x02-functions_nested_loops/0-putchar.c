cc77bdfbc69301e1f7ff70c28d791abf4239e733
ribiliamin <bayo.biliamin15@kwasu.edu.ng>
2023-09-12 11:37:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-int main() {
-  printf("_putchar\n");
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
