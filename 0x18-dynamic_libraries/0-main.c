3ec1a2f15828ffc5530a484fc3a2c5f73f1e552e
ribiliamin <bayo.biliamin15@kwasu.edu.ng>
2023-12-11 15:05:34 -0800

    0-main.c

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,3 @@
+## 0x18. C - Dynamic libraries
+0. A library is not a luxury but one of the necessities of life
+# 1. Without libraries what have we? We have no past and no future
